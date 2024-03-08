c9ef54ef1b23b3d322069e3ef08b48a413384971
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-08 04:22:04 +0000

    crt0: setup initial stack

--- arch/arm64/crt0.s
+++ arch/arm64/crt0.s
@@ -9,14 +9,23 @@ _start:
 	mov x29, xzr
 	mov x30, xzr
 
+	// Mask interrupts
+	msr DAIFset, #0b1111
+
 	// Preserve boot arguments from Helix
 	mov x20, x0
 	mov x21, x1
 	mov x22, x2
 	mov x23, x3
 
-	// Mask interrupts
-	msr DAIFset, #0b1111
+	// Helix will have setup temporary page tables
+	// to get us into virtual memory. We'll need to
+	// setup new initial page tables :^)
+	bl mmu_init
+
+	// Setup stack
+	ldr x0, =__stack_end
+	mov sp, x0
 
 	// Set vector table
 	adr x0, vector_table
